8699b13730db6f3eeb72773f4c90db6878b1a6ed
AravaTanish <aravatanish@gmail.com>
2025-10-31 09:00:26 +0530

    Completed the payments page with a dummy payment simulation and minor changes

--- controllers/booking.js
+++ controllers/booking.js
@@ -17,7 +17,32 @@ module.exports.getBookingPage = async (req, res) => {
   res.render("booking/booking.ejs", { listing, no_of_reviews, avgRating });
 };
 
-module.exports.getPaymentsPage = async (req, res) => {
-  let { startDate, endDate, adults, children } = req.params;
-  res.render("booking/payment.ejs", { startDate, endDate, adults, children });
+module.exports.postPaymentsPage = async (req, res) => {
+  let { id } = req.params;
+  let { startDate, endDate, adults, children } = req.body;
+  if (adults < 1) {
+    req.flash("error", "Adults should be at least 1!");
+    return res.redirect(`/${id}/book`);
+  }
+  if (children < 0) {
+    req.flash("error", "Children should be at least 0!");
+    return res.redirect(`/${id}/book`);
+  }
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const diffTime = end.getTime() - start.getTime();
+  const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  if (days < 0) {
+    req.flash("error", "End date should be after the start date!");
+    return res.redirect(`/${id}/book`);
+  }
+
+  const listing = await Listing.findById(id);
+  res.render("booking/payment.ejs", {
+    days,
+    adults,
+    children,
+    listing,
+  });
 };
--- routes/booking.js
+++ routes/booking.js
@@ -2,17 +2,12 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn } = require("../middleware.js");
-//const { validateListing } = require("../middleware.js");
 const bookingController = require("../controllers/booking.js");
-const { storage } = require("../cloudConfig.js");
-const multer = require("multer");
-const upload = multer({ storage });
 
 router.get("/", isLoggedIn, wrapAsync(bookingController.getBookingPage));
-router.get(
+router.post(
   "/payment",
   isLoggedIn,
-  wrapAsync(bookingController.getPaymentsPage)
+  wrapAsync(bookingController.postPaymentsPage)
 );
-
 module.exports = router;
